42cb92e7392559e36116c32667d0ac47418bd726
SothanaV <nai6320@gmail.com>
2017-07-30 12:26:54 +0700

    add arduino

--- flask/main.py
+++ flask/main.py
@@ -5,9 +5,16 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 
 socketio = SocketIO(app)
+@app.route("/bohin")
+def web1():
+	return render_template('bohin.html')
 
-
-
+@app.route("/data/<temp>/<humi>")
+def getdata(temp,humi):
+	print("Temperature:%s Humidity:%s"%(temp,humi))
+	#socketio.emit('s2c_temp',temp)
+	#socketio.emit('s2c_humi',humi)
+	return "OK"
 
 if __name__ == "__main__":
 	socketio.run(app)
